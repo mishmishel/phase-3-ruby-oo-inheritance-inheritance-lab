ee82e87811b1f37ebf5911328c44dc948ff7ef3d
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-09-18 17:57:51 +1000

    commit tree 9e586442f89e529d599f858ed1407bfffdcea74e

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative 'user'
 
+class Student < User
+    
+    def initialize
+        @knowledge = [] # Initialize @knowledge as an empty array
+    end
+    
+    def learn(knowledge_string)
+        @knowledge << knowledge_string
+    end
+    
+    def knowledge
+        @knowledge
+    end
 end
